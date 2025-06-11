0948a0fe7d2b923a7a46b0ade7a1318980656d5b
XcstPhonk <Egroth@ridgewoodschool.org>
2025-06-11 15:38:31 -0400

    Create weapons.js

---
+++ weapons.js
@@ -0,0 +1,13 @@
+// weapons.js
+
+export const weaponKits = [
+  { id: 'splattershot', name: 'Splattershot' },
+  { id: 'splatroller', name: 'Splat Roller' },
+  { id: 'splatcharger', name: 'Splat Charger' },
+  { id: 'slosher', name: 'Slosher' },
+  { id: 'dualies', name: 'Dualies' },
+  { id: 'blaster', name: 'Blaster' },
+  { id: 'brush', name: 'Inkbrush' },
+  { id: 'sloshing-machine', name: 'Sloshing Machine' },
+  // Add more weapons as you want
+];
