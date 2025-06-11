16be546571254567c2be9c973979930abcc29d64
XcstPhonk <Egroth@ridgewoodschool.org>
2025-06-11 17:06:46 -0400

    Create hero-maps.js

---
+++ hero-maps.js
@@ -0,0 +1,11 @@
+// hero-maps.js
+export const heroMaps = [
+  {
+    name: 'Side Order Tower - Floor 1',
+    background: 'linear-gradient(#fff, #ddd)',
+  },
+  {
+    name: 'Octo Expansion - Deepsea Line',
+    background: 'linear-gradient(#112, #445)',
+  }
+];
